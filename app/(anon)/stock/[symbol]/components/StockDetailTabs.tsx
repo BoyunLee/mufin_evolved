23dd38fb557059546cb6a49aab36f6528a5e22eb
BoyunLee <qhdbs0802@naver.com>
2025-03-21 01:02:08 +0900

    fix

--- app/(anon)/stock/[symbol]/components/StockDetailTabs.tsx
+++ app/(anon)/stock/[symbol]/components/StockDetailTabs.tsx
@@ -45,7 +45,7 @@ const StockDetailTabs = ({ symbol, initialPrice }: StockDetailTabsProps) => {
   };
 
   const handleTradeClick = () => {
-    if (marketOpen()) {
+    if (!marketOpen()) {
       setIsModalOpen(true);
     } else {
       sessionStorage.setItem('initialPrice', initialPrice);
