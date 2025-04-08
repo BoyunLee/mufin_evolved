4750c555adbc2646efae955b1c445870f595cf59
BoyunLee <qhdbs0802@naver.com>
2025-04-08 22:34:37 +0900

    fix : build error

--- app/api/approval-key/route.ts
+++ app/api/approval-key/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { NextRequest, NextResponse } from "next/server";
 
 import { IAcquireApprovalKeyUseCase } from "@/application/usecases/kis/interfaces/IAcquireApprovalKeyUseCase";
