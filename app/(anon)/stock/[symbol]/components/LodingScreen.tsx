742f79d3c3e1de15867c6cbc57d9493375ae2912
BoyunLee <qhdbs0802@naver.com>
2025-04-15 17:49:54 +0900

    fix : build error

--- app/(anon)/stock/[symbol]/components/LodingScreen.tsx
+++ app/(anon)/stock/[symbol]/components/LodingScreen.tsx
@@ -3,7 +3,7 @@ import { SyncLoader } from "react-spinners";
 
 const LoadingScreen = () => (
     <ScreenSection>
-      <p>차트 데이터를 불러오는 중입니다</p>
+      <p>데이터를 불러오는 중입니다</p>
       <SyncLoader color="#5865f2" margin={10} />
     </ScreenSection>
   );
